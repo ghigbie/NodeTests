e97c8d0900c06b18fa474f9f04bf80945d733d25
George Higbie <gehigbie@gmail.com>
2017-03-30 12:43:07 -0700

    set up test in package.json

---
+++ utils/utils.test.js
@@ -0,0 +1,5 @@
+const utils = require("./utils");
+
+it("should add two numbers", () => {
+    var res = utils.add(33, 11);
+}); //takes two arguments - the first is a description, and next is a function 
