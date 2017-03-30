d7e95d0d65d9a0b15c1b22eaed8dcce826f3d298
George Higbie <gehigbie@gmail.com>
2017-03-30 12:47:56 -0700

    add conditional to test function

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,4 +2,7 @@ const utils = require("./utils");
 
 it("should add two numbers", () => {
     var res = utils.add(33, 11);
-}); //takes two arguments - the first is a description, and next is a function 
+    if( res !== 44){
+        throw new Error(`Expected 44, but got ${res}`);
+    }
+}); //takes two arguments - the first is a description, and next is a function
