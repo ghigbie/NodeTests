cde891c30f3cd71acced6694ad741d23c7d08f24
George Higbie <gehigbie@gmail.com>
2017-03-30 12:56:35 -0700

    create new fucntion and test case

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,7 +2,14 @@ const utils = require("./utils");
 
 it("should add two numbers", () => {
     var res = utils.add(33, 11);
-    if( res !== 44){
+    if(res !== 44){
         throw new Error(`Expected 44, but got ${res}`);
     }
 }); //takes two arguments - the first is a description, and next is a function
+
+it("should square a number", () => {
+    var res = utils.sqaure(8);
+    if(res !=== 64){
+        throw new Errot(`Expected 64, but got ${res}`);
+    }
+});
