47654f3480297f2860c54b42c34b75ebf53e3306
George Higbie <gehigbie@gmail.com>
2017-03-30 21:04:55 -0700

    modify test case

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,14 +3,14 @@ const expect = require("expect"),
 
 it("should add two numbers", () => {
     var res = utils.add(33, 11);
-    if(res !== 44){
-        throw new Error(`Expected 44, but got ${res}`);
-    }
-}); //takes two arguments - the first is a description, and next is a function
+    console.log("*******EXPECT RUNNING*********");
+    expect(res).toBe(40);
+    // console.log("******************************");
 
-console.log("*******EXPECT RUNNING*********");
-expect(res).toBe(44);
-console.log("******************************");
+    // if(res !== 44){
+    //     throw new Error(`Expected 44, but got ${res}`);
+    // }
+}); //takes two arguments - the first is a description, and next is a function
 
 it("should square a number", () => {
     var res = utils.square(8);
