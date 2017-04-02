26014601bcceaa6f939b7195e8e0ef254fe11ee3
George Higbie <gehigbie@gmail.com>
2017-04-02 12:44:46 -0700

    Add describe blocks

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,33 @@ const expect = require("expect");
 
 var app = require("./server").app;
 
-it("should return Yo response", (done) =>{
-    request(app)
-        .get("/")
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: "Page not found",
-            });
-        })
-        .end(done);
-});
+describe("Server", () => { 
+    describe("#GET /", () => {
+        it("should return Yo response", (done) =>{
+            request(app)
+                .get("/")
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: "Page not found",
+                    });
+                })
+                .end(done);
+        });
+    });//end of describe block
 
-it("should return an object of users", (done) => {
-    request(app)
-        .get("/users")
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: "Billy",
-                age: 34
-            });
-        })
-        .end(done);
-});
+    describe("GET /users", () => {
+        it("should return an object of users", (done) => {
+            request(app)
+                .get("/users")
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: "Billy",
+                        age: 34
+                    });
+                })
+                .end(done);
+        });
+    });//end of describe block
+});//end of describe block
