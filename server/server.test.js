0f6fd8d1f018056972f9e5e5802398b3ca1bad2a
George Higbie <gehigbie@gmail.com>
2017-04-02 11:51:59 -0700

    Add new test case for new route

--- server/server.test.js
+++ server/server.test.js
@@ -6,8 +6,18 @@ it("should return Yo response", (done) =>{
     request(app)
         .get("/")
         .expect(404)
-        .expect({
-            error: "Page not found"
+        .expect(res.body).toInclude({
+            error: "Page not found",
+            name: "Todo App v1.0"
+            });
         })
+        .end(done);
+});
+
+it("should redturn an object of users", (done) => {
+    request(app)
+        .get("/users")
+        .expect(200)
+        .expect(res.body).toBeA(object)
         .end(done);
 });
