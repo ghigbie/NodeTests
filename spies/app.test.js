f8e956dcadbebed12cfc33d4cbf83b9d88edae0f
George Higbie <gehigbie@gmail.com>
2017-04-03 18:35:31 -0700

    Install rewire

--- spies/app.test.js
+++ spies/app.test.js
@@ -3,7 +3,7 @@ const expect = require("expect");
 describe("App", () => {
     it("should call the spy correctly", () => {
         var spy = expect.createSpy();
-        spy();
-        expect(spy).toHaveBeenCalled();
+        spy("George", 38);
+        expect(spy).toHaveBeenCalledWith("George", 38);
     });
 });
